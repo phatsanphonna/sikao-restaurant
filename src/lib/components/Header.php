85b69a11225f3dee643f5dca2273cb007bd6c1bd
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2024-02-24 23:48:04 +0700

    Add fonts and styles to Header.php and update index.php

---
+++ src/lib/components/Header.php
@@ -0,0 +1,23 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@100;200;300;400;500;600;700&family=Noto+Sans+Thai:wdth,wght@62.5..100,100..900&display=swap" rel="stylesheet">
+<script src="https://cdn.tailwindcss.com"></script>
+<script>
+  tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#FB8020',
+          secondary: '#B7B7B7',
+          surface: '#FFF',
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  body {
+    font-family: "Noto Sans Thai", sans-serif;
+  }
+</style>
--- src/web/index.php
+++ src/web/index.php
@@ -4,12 +4,18 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php include '../lib/components/Header.php'; ?>
   <title>Document</title>
 </head>
 
 <body>
   <?php include '../lib/components/Navbar.php'; ?>
-  <h1>Home</h1>
+  
+  <?php echo $_GET['q']; ?>
+
+  <h1 class="button">
+    Hello world!
+  </h1>
 </body>
 
 </html>
