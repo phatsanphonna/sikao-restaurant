65189851c6a54e5e309212ae811bd88bee87b6ee
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2024-02-26 17:04:23 +0700

    Add heading for kitchen orders

--- src/web/kitchen.php
+++ src/web/kitchen.php
@@ -12,13 +12,22 @@ if ($_SESSION['user']['user_role'] !== 'CHEF') {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php include '../lib/components/Header.php'; ?>
   <title>Kitchen - Si Kao Restaurant</title>
 </head>
+
 <body>
   <?php include '../lib/components/Navbar.php'; ?>
+
+  <main class="p-4 flex flex-col gap-6">
+    <h2 class="text-secondary text-left text-4xl font-bold">
+      รายการอาหารที่ต้องทำ
+    </h2>
+  </main>
 </body>
+
 </html>
