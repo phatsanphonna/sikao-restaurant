4f8fad8e84475782e0e59fa0acd1444779138b97
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2024-02-26 21:05:34 +0700

    Add screen styling and logo to index.php

--- src/web/index.php
+++ src/web/index.php
@@ -14,10 +14,19 @@ if (!isset($_SESSION['user'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php include '../lib/components/Header.php'; ?>
   <title>Document</title>
+  <style>
+    .screen {
+      height: calc(100vh - 6rem);
+    }
+  </style>
 </head>
 
 <body>
   <?php include '../lib/components/Navbar.php'; ?>
+
+  <main class="screen p-4 flex flex-col gap-6 justify-center items-center">
+    <?php include '../lib/components/LogoLarge.php'; ?>
+  </main>
 </body>
 
 </html>
