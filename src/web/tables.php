7d53d881d2f819680f3151fd1c8ef73f1b1df5f4
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2024-03-04 17:06:14 +0700

    Fix: Scrollable current table in tables.php

--- src/web/tables.php
+++ src/web/tables.php
@@ -14,14 +14,19 @@ if (!isset($_SESSION['user'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php include '../lib/components/Header.php'; ?>
   <title>โต๊ะ - Si Kao Restaurant</title>
+  <style>
+    .screen {
+      height: calc(100vh - 8rem);
+    }
+  </style>
 </head>
 
 <body>
   <?php include '../lib/components/Navbar.php'; ?>
 
   <?php if (!isset($_GET['table_id'])) { ?>
-    <main class="p-4 flex gap-6 divide-x">
-      <aside class="h-full">
+    <main class="p-4 flex gap-6">
+      <aside class="screen overflow-y-auto w-96 flex flex-col gap-4">
         <?php
         $sql = "SELECT * FROM res_order ro JOIN res_table rt ON (ro.table_id = rt.table_id) WHERE ro.checkout_at IS NULL";
         $result = $conn->query($sql);
@@ -29,7 +34,7 @@ if (!isset($_SESSION['user'])) {
         if ($result->num_rows > 0) {
           while ($row = $result->fetch_assoc()) {
         ?>
-            <a href="tables.php?table_id=<?php echo $row['table_id'] ?>" class="rounded-lg w-72 h-40 shadow-lg flex flex-col justify-between text-white bg-primary p-4 mb-10">
+            <a href="tables.php?table_id=<?php echo $row['table_id'] ?>" class="rounded-lg h-40 shadow-lg flex flex-col justify-between text-white bg-primary p-4">
               <h4 class="text-8xl font-bold text-left">
                 <?php echo $row['table_name'] ?>
               </h4>
@@ -42,22 +47,24 @@ if (!isset($_SESSION['user'])) {
         } ?>
       </aside>
 
-      <div class="grid grid-cols-3 w-full gap-2 place-items-center">
-        <?php
-        $sql = "SELECT * FROM res_table";
-        $result = $conn->query($sql);
+      <div class="w-full screen overflow-y-auto">
+        <div class="grid grid-cols-3 w-full gap-2 place-items-center h-full">
+          <?php
+          $sql = "SELECT * FROM res_table";
+          $result = $conn->query($sql);
 
-        if ($result->num_rows > 0) {
-          while ($row = $result->fetch_assoc()) {
-            $sql = "SELECT * FROM res_order WHERE table_id = " . $row['table_id'] . " AND checkout_at IS NULL";
-            $active_result = $conn->query($sql);
-            $is_active = $active_result->num_rows > 0;
-        ?>
-            <a href="tables.php?table_id=<?php echo $row['table_id'] ?>" class="rounded-2xl w-40 h-20 text-center flex justify-center items-center text-2xl <?php $is_active ? print('text-white bg-primary') : print('text-black bg-stone-200') ?>">
-              <?php echo $row['table_name'] ?>
-            </a>
-        <?php }
-        } ?>
+          if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+              $sql = "SELECT * FROM res_order WHERE table_id = " . $row['table_id'] . " AND checkout_at IS NULL";
+              $active_result = $conn->query($sql);
+              $is_active = $active_result->num_rows > 0;
+          ?>
+              <a href="tables.php?table_id=<?php echo $row['table_id'] ?>" class="rounded-2xl w-40 h-20 text-center flex justify-center items-center text-2xl <?php $is_active ? print('text-white bg-primary') : print('text-black bg-stone-200') ?>">
+                <?php echo $row['table_name'] ?>
+              </a>
+          <?php }
+          } ?>
+        </div>
       </div>
     </main>
   <?php } else { ?>
