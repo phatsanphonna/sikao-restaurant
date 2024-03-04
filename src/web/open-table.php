9d154c5620a25ea11de7ec2a2be73208e4e741e1
Phatsanphon Nakaranurak <phatsanphon2004@outlook.com>
2024-03-04 17:36:28 +0700

    Feat: init checkout.php

--- src/web/open-table.php
+++ src/web/open-table.php
@@ -16,14 +16,31 @@ $table_id = $_GET['table_id'];
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <?php include '../lib/components/Header.php'; ?>
+  <style>
+    .screen {
+      height: calc(100vh - 8rem);
+    }
+  </style>
 </head>
 
 <body>
   <?php include '../lib/components/Navbar.php'; ?>
-  <main class="p-4 flex flex-col gap-6">
-    <form action="add-people-to-table.php" method="POST" class="flex flex-col gap-4 items-center">
-      <input tpye="hidden" name="table_id" value="<?php echo $table_id ?>">
-      จำนวนคนที่นั่ง
+  <main class="p-4 flex flex-col gap-6 container mx-auto">
+    <?php
+    $sql = "SELECT * FROM res_table WHERE table_id = $table_id";
+    $result = $conn->query($sql);
+    $table = $result->fetch_assoc();
+    ?>
+
+    <header>
+      <h4 class="text-8xl font-bold"><?php echo $table['table_name'] ?></h4>
+      <hr>
+    </header>
+
+    <form action="add-people-to-table.php" method="POST" class="flex flex-col gap-4 items-center justify-center h-full">
+      <p class="text-4xl">จำนวนคนที่นั่ง</p>
+
+      <input hidden name="table_id" value="<?php echo $table_id ?>">
 
       <div class="flex justify-center items-center gap-2 w-3/5 mx-auto">
         <button type="button" class="bg-surface text-black px-5 py-2 rounded-full text-2xl" onclick="document.getElementById('amount').stepDown()">-</button>
